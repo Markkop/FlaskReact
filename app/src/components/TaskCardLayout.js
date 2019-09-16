17056198537afbf7271f8074dc2725e9b299f925
Mark Kop <mk.marcelo.mk@gmail.com>
2019-09-15 22:10:48 -0300

    added isDue color feature

--- app/src/components/TaskCardLayout.js
+++ app/src/components/TaskCardLayout.js
@@ -10,35 +10,46 @@ const TaskCardLayout = ({
   buttonText,
   handleChange
 }) => {
+  // enables isDue css class for older dates (had to split to get locale)
+  const isDue = deadline && new Date([...deadline.split("-")]) - new Date() < 0;
   return (
     <form onSubmit={handleClick} className="taskContainer">
       <div className="taskCard">
         {/* <p>TaskID: {id}</p> */}
         <div className="formPair">
           <label htmlFor="title">Title:</label>
-          {title ? (
-            <p>{title}</p>
-          ) : (
+          {title && <p>{title}</p>}
+          {!title && (
             <input type="text" id="title" onChange={handleChange}></input>
           )}
         </div>
         <div className="formPair">
           <label htmlFor="description">Description:</label>
-          {description ? (
-            <p>{description}</p>
-          ) : (
+          {description && <p>{description}</p>}
+          {!description && (
             <input type="text" id="description" onChange={handleChange}></input>
           )}
         </div>
         <div className="formPair">
           <label htmlFor="deadline">Deadline:</label>
-          {deadline ? (
-            <p>{deadline}</p>
-          ) : (
+          {deadline && (
+            <p className={`${isDue && !completedAt && "isDue"}`}>{deadline}</p>
+          )}
+          {!deadline && (
             <input type="date" id="deadline" onChange={handleChange}></input>
           )}
         </div>
-        {completedAt && <p>{`Completed at: ${completedAt}`}</p>}
+        <div className="formPair">
+          {completedAt && <p>Completed on:</p>}
+          {completedAt && (
+            <p>
+              {completedAt
+                .split(" ")
+                .splice(0, 5)
+                .join(" ")}
+            </p>
+          )}
+        </div>
       </div>
       <button onClick={handleClick}>{buttonText}</button>
     </form>
